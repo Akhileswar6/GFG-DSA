58d7ccd5b7c9736f2b61a1cf51d42b32c7e5e2c5
Akhileswar <akhilkamale@gmail.com>
2025-06-26 21:33:05 +0530

    initial commit

--- Arrays/3-Reverse Array.py
+++ Arrays/3-Reverse Array.py
@@ -1,20 +1,20 @@
 # Reverse an array in place
 
 # Approach-1
-# class Solution:
-#     def reverseArray(self, arr):
-#         left = 0
-#         right = len(arr) -1
+class Solution:
+    def reverseArray(self, arr):
+        left = 0
+        right = len(arr) -1
 
-#         while left < right:
-#             arr[left], arr[right] = arr[right], arr[left]
-#             left +=1
-#             right -=1
-#         return arr
+        while left < right:
+            arr[left], arr[right] = arr[right], arr[left]
+            left +=1
+            right -=1
+        return arr
 
 
-# s = Solution()
-# print(s.reverseArray([87,12,65,0,23,34,89,90]))
+s = Solution()
+print(s.reverseArray([87,12,65,0,23,34,89,90]))
 
 
 # Approach-2
