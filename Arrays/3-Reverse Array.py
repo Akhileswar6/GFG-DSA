e795c4969624acb9d128ae6f76964c848cb3de9e
Akhileswar <akhilkamale@gmail.com>
2025-06-27 09:17:09 +0530

    initial commit

--- Arrays/3-Reverse Array.py
+++ Arrays/3-Reverse Array.py
@@ -3,13 +3,14 @@
 # Approach-1
 class Solution:
     def reverseArray(self, arr):
-        left = 0
-        right = len(arr) -1
+        left = 0                                                    # Holds the index of the first element
+        right = len(arr) -1                                         # Holds the index of the last element
 
-        while left < right:
-            arr[left], arr[right] = arr[right], arr[left]
-            left +=1
-            right -=1
+        while left < right:                                         # loop until the two pointers meet
+            arr[left], arr[right] = arr[right], arr[left]           # arr[87], arr[90] = arr[90], arr[87]
+
+            left +=1                                                # increment the left pointer
+            right -=1                                               # decrement the right pointer
         return arr
 
 
@@ -20,10 +21,10 @@ print(s.reverseArray([87,12,65,0,23,34,89,90]))
 # Approach-2
 class Solution:
     def reverseArray(self, arr):
-        n = len(arr)
+        n = len(arr)                                              # Get the length of the array
         
-        for i in range(n//2):
-            arr[i], arr[n-i-1] = arr[n-i-1], arr[i]
+        for i in range(n//2):                                     # Loop through the first half of an array
+            arr[i], arr[n-i-1] = arr[n-i-1], arr[i]               # arr [87], arr[9-0-1] = arr[9-0-1], arr[87]
         return arr
 
 s = Solution()
