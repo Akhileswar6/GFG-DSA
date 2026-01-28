5ccf641c15ec6c226d4ea1bb688056b766859568
Akhileswar Kamale <akhilkamale@gmail.com>
2026-01-28 15:24:12 +0530

    updated code

--- Arrays/1-Second-Largest.py
+++ Arrays/1-Second-Largest.py
@@ -2,22 +2,18 @@
 
 class Solution:
     def getSecondLargest(self, arr):
-        n = len(arr)
-        if n < 2:
-           return -1        # returns -1 if there are less than 2 elements
-           
         first = second = float('-inf')
         for num in arr:
-            if num > first:          # if 23 > first:
-                second = first       # second = ('-inf')
-                first = num          # first = 23
-            elif num > second and num != first:    # if 65 > 23  and 65 != 87:
-                second = num                       # second = 65
-                
-        if second == float('-inf'):     # if second = -inf
-            return -1                   # return -1 if no second largest found
-        else:
-            return second
+            if num > first:
+                second = first
+                first = num
+
+            elif first > num > second:
+                second = num
+
+        return second
+
+        
 
 s = Solution()
 print(s.getSecondLargest([23,87,65,12,34,89,90]))  
--- Arrays/2-Move-all-zeros-to-end.py
+++ Arrays/2-Move-all-zeros-to-end.py
@@ -2,13 +2,13 @@
 
 class Solution:
     def pushZerosToEnd(self, arr):
-        count = 0       # position to place the next non-zero element
+        count = 0 
         n = len(arr)    
 
         for i in range(n):
-            if arr[i] != 0:                                  # if 5 != 0:                         
-                arr[i], arr[count] = arr[count], arr[i]      # arr[1], arr[0] = arr[0], arr[1]    
-                count += 1                                   # increment count to 1
+            if arr[i] != 0:
+                arr[i], arr[count] = arr[count], arr[i] 
+                count += 1
 
 
 
